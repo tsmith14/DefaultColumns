0e2fcb96b9271931e7da6f2081ce934417d752a6
Tyler Smith <tylertsmith14@gmail.com>
2014-02-18 01:14:58 -0500

    Fixing Migration Template

--- lib/default_columns/generators/active_record/model/model_generator.rb
+++ lib/default_columns/generators/active_record/model/model_generator.rb
@@ -8,8 +8,8 @@ module DefaultColumns
     	
 #    	  source_root File.expand_path("../../migration/templates", __FILE__)
     	   
-      	class_option :archivable,  :type => :boolean
-      	class_option :deletable, :type => :boolean
+#      	class_option :archivable,  :type => :boolean
+#      	class_option :deletable, :type => :boolean
 	  	end
   	end
 	end
