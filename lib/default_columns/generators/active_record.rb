0a2348ec4935c03c547308948668b3b88fca12f4
Tyler Smith <tylertsmith14@gmail.com>
2014-02-18 00:11:45 -0500

    Fixing Migration Template

--- lib/default_columns/generators/active_record.rb
+++ lib/default_columns/generators/active_record.rb
@@ -11,7 +11,7 @@ module DefaultColumns
   end
 end
 
-Rails::Generators::Base.extend DefaultColumns::ActiveRecord::Generators::Base
+Rails::Generators::NamedBase.extend DefaultColumns::ActiveRecord::Generators::Base
 Rails::Generators::ActiveRecord::Model::ModelGenerator.send :include, DefaultColumns::Generators::ActiveRecord::Model::ModelGenerator
 
 
