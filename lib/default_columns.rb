68a095cc4cad2eaaf8302ac8f92dddaeaadbdeb3
Tyler Smith <tylertsmith14@gmail.com>
2014-02-17 23:48:34 -0500

    Replaced Migration Template

--- lib/default_columns.rb
+++ lib/default_columns.rb
@@ -1,6 +1,13 @@
 module DefaultColumns
 	autoload :Schema, 'default_columns/schema'
 	autoload :Models, 'default_columns/models'
+	
+	class Railtie < Rails::Railtie
+		config.app_generators do |g|
+	  	g.templates.unshift File::expand_path('generators/active_record/migration/templates', __FILE__)
+	  end 
+	end
+	  
 end
 require 'default_columns/active_record'
 #require 'default_columns/schema'
